32c65380f76a7ca89c07bfaff3c079ee6225d80b
cartmedia <info@cartmedia.nl>
2023-12-12 22:21:35 +0100

    clear

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ["src/**/*.{js,ts,html,css,scss}"],
- theme: {
-    fontFamily: {
-        'sans': ['Helvetica', 'Arial', 'sans-serif'],
-    },
-  },
-  plugins: [],
-};
